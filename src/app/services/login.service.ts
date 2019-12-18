4a35a6d869a70c02c2c180cd93cfe6a11235b286
Anand T <anand.thyagaraju@gmail.com>
2019-12-19 01:23:26 +0530

    configured clearTextInput for android 9 to support http communication

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -14,9 +14,12 @@ export class LoginService {
 
     const payload = { mobile_no };
 
+    console.log( 'payload-->>', payload );
+
     const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
 
     return this.httpClient.post(
+              // 'http://13.234.250.245/g2d/api/delivery_api.php?function=deliveryLogin',
               'http://newwavestech.com/grocery/api/delivery_api.php?function=deliveryLogin' ,
               payload ,
               // {responseType: 'text', headers}
